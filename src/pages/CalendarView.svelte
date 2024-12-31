7deb2bf5a7e2909a9ca23aaef67ae351a09d45c8
Haunted Bees <fench@hauntedbees.com>
2024-12-30 18:57:12 -0800

    closes #5

--- src/pages/CalendarView.svelte
+++ src/pages/CalendarView.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-    import dayjs from "dayjs";
+	import dayjs from "dayjs";
 	import ArrowLeft from "svelte-material-icons/ArrowLeft.svelte";
 	import ArrowRight from "svelte-material-icons/ArrowRight.svelte";
-    import { FormatDosage, type Medicine, type MedicineTaken } from "../lib/Models";
-    import db from "../lib/Data";
+	import {
+		FormatDosage,
+		type Medicine,
+		type MedicineTaken,
+	} from "../lib/Models";
+	import db from "../lib/Data";
 
 	interface MedicineAmount {
 		medicineId: number;
@@ -12,70 +16,80 @@
 		dosageUnit: string;
 		details: MedicineTaken[];
 	}
-	let medicines: {[key: number]: Medicine} = {};
-	db.medicine.toArray().then(ms => {
-		ms.forEach(m => medicines[m.id || 0] = m);
+	let medicines: { [key: number]: Medicine } = {};
+	db.medicine.toArray().then((ms) => {
+		ms.forEach((m) => (medicines[m.id || 0] = m));
 	});
 
 	let currentMonth = dayjs().startOf("month");
 	let previousMonth = currentMonth.subtract(1, "month");
 	let nextMonth = currentMonth.add(1, "month");
 	let weekStarts: dayjs.Dayjs[] = [];
-	let medicineDictionary: {[key: string]: MedicineAmount[]} = {};	
+	let medicineDictionary: { [key: string]: MedicineAmount[] } = {};
 
 	function UpdateMonth(change: number) {
 		currentMonth = currentMonth.add(change, "month");
 		previousMonth = currentMonth.subtract(1, "month");
 		nextMonth = currentMonth.add(1, "month");
 		weekStarts = [currentMonth.startOf("week")];
-		while(true) {
+		while (true) {
 			const next = weekStarts[weekStarts.length - 1].add(1, "week");
-			if(next.isSame(currentMonth, "month")) {
+			if (next.isSame(currentMonth, "month")) {
 				weekStarts.push(next);
 			} else {
 				break;
 			}
 		}
-		db.taken.where("timeTaken").between(
-			currentMonth.toDate(), 
-			currentMonth.endOf("month").toDate(), 
-			true, 
-			false
-		).toArray().then(ms => {
-			medicineDictionary = {};
-			ms.forEach(m => {
-				const key = dayjs(m.timeTaken).startOf("day").format();
-				if(medicineDictionary[key]) {
-					let found = false;
-					for(let i = 0; i < medicineDictionary[key].length; i++) {
-						const med = medicineDictionary[key][i];
-						if(med.medicineName === m.medicineName) {
-							found = true;
-							med.dosageAmount += m.dosageAmount;
-							med.details.push(m);
-							break;
+		db.taken
+			.where("timeTaken")
+			.between(
+				currentMonth.toDate(),
+				currentMonth.endOf("month").toDate(),
+				true,
+				false,
+			)
+			.toArray()
+			.then((ms) => {
+				medicineDictionary = {};
+				ms.forEach((m) => {
+					const key = dayjs(m.timeTaken).startOf("day").format();
+					if (medicineDictionary[key]) {
+						let found = false;
+						for (
+							let i = 0;
+							i < medicineDictionary[key].length;
+							i++
+						) {
+							const med = medicineDictionary[key][i];
+							if (med.medicineName === m.medicineName) {
+								found = true;
+								med.dosageAmount += m.dosageAmount;
+								med.details.push(m);
+								break;
+							}
 						}
+						if (!found) {
+							medicineDictionary[key].push({
+								medicineId: m.medicineId,
+								medicineName: m.medicineName,
+								dosageAmount: m.dosageAmount,
+								dosageUnit: m.dosageUnit,
+								details: [m],
+							});
+						}
+					} else {
+						medicineDictionary[key] = [
+							{
+								medicineId: m.medicineId,
+								medicineName: m.medicineName,
+								dosageAmount: m.dosageAmount,
+								dosageUnit: m.dosageUnit,
+								details: [m],
+							},
+						];
 					}
-					if(!found) {
-						medicineDictionary[key].push({
-							medicineId: m.medicineId,
-							medicineName: m.medicineName,
-							dosageAmount: m.dosageAmount,
-							dosageUnit: m.dosageUnit,
-							details: [m]
-						});
-					}
-				} else {
-					medicineDictionary[key] = [{
-						medicineId: m.medicineId,
-						medicineName: m.medicineName,
-						dosageAmount: m.dosageAmount,
-						dosageUnit: m.dosageUnit,
-						details: [m]
-					}];
-				}
+				});
 			});
-		});
 	}
 	function WeekArray(startOfWeek: dayjs.Dayjs): dayjs.Dayjs[] {
 		return [
@@ -85,11 +99,12 @@
 			startOfWeek.add(3, "day"),
 			startOfWeek.add(4, "day"),
 			startOfWeek.add(5, "day"),
-			startOfWeek.add(6, "day")
+			startOfWeek.add(6, "day"),
 		];
 	}
 	UpdateMonth(0);
 </script>
+
 <table class="table is-fullwidth is-narrow">
 	<thead class="thead">
 		<tr>
@@ -100,7 +115,9 @@
 					</span>
 				</button>
 			</th>
-			<th class="has-text-centered" colspan="5">{currentMonth.format("MMMM")}</th>
+			<th class="has-text-centered" colspan="5"
+				>{currentMonth.format("MMMM YYYY")}</th
+			>
 			<th class="has-text-centered">
 				<button on:click={() => UpdateMonth(1)}>
 					<span class="icon">
@@ -124,9 +141,23 @@
 			<tr>
 				{#each WeekArray(week) as day}
 					<td>
-						<div class="has-text-right {day.isSame(currentMonth, "month") ? "" : "has-text-light"}">{day.format("D")}</div>
+						<div
+							class="has-text-right {day.isSame(
+								currentMonth,
+								'month',
+							)
+								? ''
+								: 'has-text-light'}"
+						>
+							{day.format("D")}
+						</div>
 						{#each medicineDictionary[day.format()] as m}
-							<div class="medinfo mb-1 p-1 has-text-black" style="background-color: {medicines[m.medicineId]?.color ?? "#FFFFFF"}">
+							<div
+								class="medinfo mb-1 p-1 has-text-black"
+								style="background-color: {medicines[
+									m.medicineId
+								]?.color ?? '#FFFFFF'}"
+							>
 								{m.medicineName} ({FormatDosage(m)})
 							</div>
 						{/each}
@@ -136,6 +167,7 @@
 		{/each}
 	</tbody>
 </table>
+
 <style>
 	.medinfo {
 		border-radius: 4px;
--- src/pages/ListView.svelte
+++ src/pages/ListView.svelte
@@ -13,12 +13,14 @@
     let searchQuery = "";
 
     let currentPatientId = 0;
-    currentPatient.subscribe(p => {
+    currentPatient.subscribe((p) => {
         currentPatientId = p.id || 0;
         GetLatestTakens();
     });
 
-    currentTakenMedicine.subscribe(_ => { GetLatestTakens() });
+    currentTakenMedicine.subscribe((_) => {
+        GetLatestTakens();
+    });
 
     function ShowMore() {
         amountToShow += 30;
@@ -26,61 +28,63 @@
     }
 
     function GetLatestTakens(reset = false) {
-        if(reset) {
+        if (reset) {
             hasMore = true;
         }
         db.taken
             .orderBy("timeTaken")
             .reverse()
-            .filter(m => {
-                if(searchQuery.trim() === "") {
+            .filter((m) => {
+                if (searchQuery.trim() === "") {
                     return true;
                 }
                 const safeQuery = searchQuery.trim().toLowerCase();
                 const dateTaken = dayjs(m.timeTaken);
                 const month = dateTaken.format("MMMM").toLowerCase();
-                if(AnyStringMatch([m.medicineName, m.notes, month], safeQuery)) {
+                if (
+                    AnyStringMatch([m.medicineName, m.notes, month], safeQuery)
+                ) {
                     return true;
                 }
                 const maybeDate = dayjs(searchQuery);
-                if(maybeDate.isValid()) {
+                if (maybeDate.isValid()) {
                     return dateTaken.isSame(maybeDate, "day");
                 }
                 return false;
             })
             .limit(amountToShow)
-            .and(m => m.patientId === currentPatientId)
+            .and((m) => m.patientId === currentPatientId)
             .toArray()
-            .then(medicines => {
-                if(medicines.length < amountToShow) {
+            .then((medicines) => {
+                if (medicines.length < amountToShow) {
                     hasMore = false;
                 }
                 takenList = [];
                 let lastBreakpoint = 0; // unset, today, yesterday, this week, this month, older
-                for(let i = 0; i < medicines.length; i++) {
+                for (let i = 0; i < medicines.length; i++) {
                     const m = medicines[i];
                     const time = dayjs(m.timeTaken);
-                    if(time.isSame(dayjs(), "day")) {
-                        if(lastBreakpoint < 1) {
+                    if (time.isSame(dayjs(), "day")) {
+                        if (lastBreakpoint < 1) {
                             takenList.push("Today");
                             lastBreakpoint = 1;
                         }
-                    } else if(time.isSame(dayjs().subtract(1, "day"), "day")) {
-                        if(lastBreakpoint < 2) {
+                    } else if (time.isSame(dayjs().subtract(1, "day"), "day")) {
+                        if (lastBreakpoint < 2) {
                             takenList.push("Yesterday");
                             lastBreakpoint = 2;
                         }
-                    } else if(time.isSame(dayjs().startOf("week"), "week")) {
-                        if(lastBreakpoint < 3) {
+                    } else if (time.isSame(dayjs().startOf("week"), "week")) {
+                        if (lastBreakpoint < 3) {
                             takenList.push("This Week");
                             lastBreakpoint = 3;
                         }
-                    } else if(time.isSame(dayjs().startOf("month"), "month")) {
-                        if(lastBreakpoint < 4) {
+                    } else if (time.isSame(dayjs().startOf("month"), "month")) {
+                        if (lastBreakpoint < 4) {
                             takenList.push("This Month");
                             lastBreakpoint = 4;
                         }
-                    } else if(lastBreakpoint < 5) {
+                    } else if (lastBreakpoint < 5) {
                         takenList.push("Older");
                         lastBreakpoint = 5;
                     }
@@ -92,10 +96,11 @@
         const time = dayjs(m.timeTaken);
         const today = dayjs().startOf("day");
         const yesterday = today.subtract(1, "day");
-        if(time.isSame(today, "day") || time.isSame(yesterday, "day")) {
+        const potentialYearSuffix = time.isSame(today, "year") ? "" : "/YY";
+        if (time.isSame(today, "day") || time.isSame(yesterday, "day")) {
             return time.format("h:mm A");
         } else {
-            return time.format("MM/DD h:mm A")
+            return time.format(`MM/DD${potentialYearSuffix} h:mm A`);
         }
     }
 </script>
@@ -119,12 +124,16 @@
         {#if typeof m === "string"}
             <h2 class="subtitle mb-2 mt-3">{m}</h2>
         {:else}
-            <div class="columns is-gapless is-vcentered is-mobile mb-0 {m.notes ? "" : "pb-4"}">
+            <div
+                class="columns is-gapless is-vcentered is-mobile mb-0 {m.notes
+                    ? ''
+                    : 'pb-4'}"
+            >
                 <div class="column is-4 is-size-7">{GetDateFormat(m)}</div>
                 <div class="column is-5">{m.medicineName}</div>
                 <div class="column is-2 is-size-7">{FormatDosage(m)}</div>
                 <div class="column is-1">
-                    <button 
+                    <button
                         on:click={() => currentTakenMedicine.set(m)}
                         class="button is-info is-small is-rounded"
                     >
@@ -135,7 +144,7 @@
                 </div>
             </div>
             {#if m.notes}
-            <div class="pb-4 is-size-7">{m.notes}</div>
+                <div class="pb-4 is-size-7">{m.notes}</div>
             {/if}
         {/if}
     {/each}
@@ -145,6 +154,7 @@
         </button>
     {/if}
 </div>
+
 <style>
     .columns {
         margin-bottom: 0;
