55de70fb29d4cb74810b6e56c70235573efb37d6
Haunted Bees <fench@hauntedbees.com>
2025-01-04 21:36:31 -0800

    closes #9

--- src/lib/Settings.ts
+++ src/lib/Settings.ts
@@ -1,41 +1,54 @@
-import db from "./Data";
+import dayjs from "dayjs";
+import db, { ExportAllData } from "./Data";
 import type { Patient } from "./Models";
 import { currentPatient } from "./State";
 
 export interface Settings {
-    lastPatient: Patient
+    lastPatient: Patient,
+    lastAutoBackup: number,
 }
 
 export async function SaveSettings(patient: Patient | undefined) {
+    const settings = await GetSettings();
     if (patient) {
-        const settings = {
-            lastPatient: patient
-        };
+        settings.lastPatient = patient;
+        StoreSettings(settings);
         localStorage.setItem("medibee-settings", JSON.stringify(settings));
     } else {
         currentPatient.subscribe(p => {
-            const settings = {
-                lastPatient: p
-            };
-            localStorage.setItem("medibee-settings", JSON.stringify(settings));
+            settings.lastPatient = p;
+            StoreSettings(settings);
         });
     }
 }
 
 export async function Initialize(): Promise<Patient> {
+    const settings = await GetSettings();
+    currentPatient.set(settings.lastPatient);
+    const now = dayjs();
+    if (!settings.lastAutoBackup || now.isAfter(dayjs(settings.lastAutoBackup).add(7, "day"))) {
+        ExportAllData(`medibee_autobackup_${now.format()}.json`);
+        settings.lastAutoBackup = now.valueOf();
+        StoreSettings(settings);
+    }
+    return settings.lastPatient;
+}
+
+async function GetSettings(): Promise<Settings> {
     const savedSettings = localStorage.getItem("medibee-settings");
     if (savedSettings) {
-        const settings: Settings = JSON.parse(savedSettings);
-        currentPatient.set(settings.lastPatient);
-        return settings.lastPatient;
+        return JSON.parse(savedSettings);
     } else {
         const newUser: Patient = { name: "New Patient", notes: "" };
         return db.patient.add(newUser).then(id => {
             newUser.id = id;
             currentPatient.set(newUser);
-            const newSettings: Settings = { lastPatient: newUser };
+            const newSettings: Settings = { lastPatient: newUser, lastAutoBackup: +new Date() };
             localStorage.setItem("medibee-settings", JSON.stringify(newSettings));
-            return newUser;
+            return newSettings;
         });
     }
+}
+function StoreSettings(settings: Settings): void {
+    localStorage.setItem("medibee-settings", JSON.stringify(settings));
 }
